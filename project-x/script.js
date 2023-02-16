9090db5005bee75b5e0029c1732feea83536c9f6
ctrangel <ctrangel@pointpark.edu>
2023-02-15 19:12:39 -0500

    changes

--- project-x/script.js
+++ project-x/script.js
@@ -1,4 +1,8 @@
-function hideSidebar() {
-    var sidebar = document.getElementById("sideBar");
-    sidebar.style.display = "none";
-}
+
+function toggleSidebar() {
+    let sidebar = document.getElementById("sideBar");
+    let menuIcon = document.getElementById("menu-icon");
+  
+    sidebar.classList.toggle("open");
+    menuIcon.classList.toggle("open");
+  }
